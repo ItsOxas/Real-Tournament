757ffaf461d319624e5d251f95f96c2c98d1d446
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 12:16:18 +0200

    Weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+	public GameObject bulletPrefab;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(bulletPrefab, transform.position, transform.rotation);
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Instantiate(bulletPrefab,transform.position,transform.rotation);
+		}
+	}
 }
-
